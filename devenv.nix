ea2d6f3b9d035622dddcea2536cb2d503ff4cb2a
Rafael Kraut <14234815+RafaelKr@users.noreply.github.com>
2024-08-14 15:45:51 +0200

    create process-compose elevated caddy repro

--- devenv.nix
+++ devenv.nix
@@ -1,39 +1,50 @@
-{ pkgs, lib, config, inputs, ... }:
-
 {
-  # https://devenv.sh/basics/
-  env.GREET = "devenv";
+  pkgs,
+  inputs,
+  ...
+}:
+let
+  pkgs-unstable = import inputs.nixpkgs-unstable { system = pkgs.stdenv.system; };
+in
+{
+  process-managers.process-compose = {
+    # required to use process-compose 1.9.0+
+    package = pkgs-unstable.process-compose;
+  };
 
-  # https://devenv.sh/packages/
-  packages = [ pkgs.git ];
+  services.caddy = {
+    enable = true;
 
-  # https://devenv.sh/languages/
-  # languages.rust.enable = true;
+    config = ''
+      {
+        # Prevents binding to port 80 which would need root privileges
+      	auto_https disable_redirects
+      }
+    '';
 
-  # https://devenv.sh/processes/
-  # processes.cargo-watch.exec = "cargo-watch";
+    virtualHosts."https://localhost:8443" = {
+      extraConfig = ''
+        root * public
 
-  # https://devenv.sh/services/
-  # services.postgres.enable = true;
+        encode zstd gzip
 
-  # https://devenv.sh/scripts/
-  scripts.hello.exec = ''
-    echo hello from $GREET
-  '';
+        file_server
 
-  enterShell = ''
-    hello
-    git --version
-  '';
+        log {
+          output stderr
+          format console
+          level ERROR
+        }
+      '';
+    };
+  };
 
-  # https://devenv.sh/tests/
-  enterTest = ''
-    echo "Running tests"
-    git --version | grep --color=auto "${pkgs.git.version}"
-  '';
-
-  # https://devenv.sh/pre-commit-hooks/
-  # pre-commit.hooks.shellcheck.enable = true;
-
-  # See full reference at https://devenv.sh/reference/options/
+  processes = {
+    # required to keep process-compose running when caddy fails
+    sleep.exec = "sleep 3600";
+    caddy.process-compose = {
+      # required to install its unique root certificate into the trust store
+      is_elevated = true;
+    };
+  };
 }
